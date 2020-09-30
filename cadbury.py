e2ff80abdbeab3ce2df266cbd0c24c8327969415
LONE-EXPLORER <56498696+LONE-EXPLORER@users.noreply.github.com>
2020-09-30 17:41:53 +0530

    Add files via upload

---
+++ cadbury.py
@@ -0,0 +1,19 @@
+def co(i , j):
+    if(i==j):
+        return(1)
+    elif(i > j):
+        i = i-j
+        return(co(i , j) + 1)
+    elif(j > i):
+        j = j-i
+        return(co(j , i) + 1)
+
+p = int(input())
+q = int(input())
+r = int(input())
+s = int(input())
+m = 0
+for i in range(p , q+1):
+    for j in range(r , s+1):
+        m += co(i , j)
+print(m)
