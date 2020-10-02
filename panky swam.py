3b47e7691c1689403121df95aa249b789a26ea6c
LONE-EXPLORER <56498696+LONE-EXPLORER@users.noreply.github.com>
2020-10-02 11:07:20 +0530

    Add files via upload

---
+++ panky swam.py
@@ -0,0 +1,20 @@
+t = int(input())
+f = str(input())
+m = str(input())
+s = t
+a  = 0
+while (s != a or m != ""):
+    c =0
+    if f[0] == m[0]:
+        f = f[1:]
+        m = m[1:]
+        a += 1
+    else:
+        c += 1
+        x = m[0]
+        m = m[1:]
+        x = m + x
+        if(c == len(m)):
+            break
+
+print(t - a)
